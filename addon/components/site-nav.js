75bcc65b2f5d6f346eb8fc9b29f410a07220c321
Chris Manson <chris@manson.ie>
2020-06-04 09:33:48 +0100

    fix lint

--- addon/components/site-nav.js
+++ addon/components/site-nav.js
@@ -1,16 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { computed, get } from '@ember/object';
+import { computed } from '@ember/object';
 import layout from '../templates/components/site-nav';
 
 export default Component.extend({
   blog: service(),
   router: service(),
-  isHome: computed('router.currentRouteName', function() {
-    return this.router.currentRouteName === 'index';
-  }),
+  isHome: computed.equal('router.currentRouteName', 'index'),
   rssFeed: computed('blog.host', function() {
-    return `https://feedly.com/i/subscription/feed${encodeURIComponent('/' + get(this, 'blog.host') + '/rss.xml')}`;
+    return `https://feedly.com/i/subscription/feed${encodeURIComponent(`/${this.blog.host}/rss.xml`)}`;
   }),
   layout
 });
