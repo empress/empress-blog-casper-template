9a75caba98ed38e942130697b978283240c54cc1
Chris Manson <chris@manson.ie>
2020-10-01 16:29:35 +0100

    use findHost to find real host app
    Co-Authored-By: Chris Manson <mansona@users.noreply.github.com>

--- index.js
+++ index.js
@@ -26,26 +26,32 @@ module.exports = {
     postcssOptions
   },
 
-  // TODO get this to work properly and remove the need for the default blueprint
-  config(env, config) {
-    if(!config['responsive-image']) {
-      return {
-        'responsive-image': {
-          sourceDir: 'images',
-          destinationDir: 'responsive-images',
-          quality: 80,
-          supportedWidths: [2000, 1000, 600, 300],
-          removeSourceDir: false,
-          justCopy: false,
-          extensions: ['jpg', 'jpeg', 'png', 'gif']
-        }
-      }
-    }
-  },
+  included() {
+    let app = findHost(this);
 
-  included(app) {
-    this._super.included.apply(this, arguments)
+    app.options['responsive-image'] = {
+      images: [{
+        include: 'images/**/*',
+        removeSource: false,
+        quality: 80,
+        widths: [2000, 1000, 600, 300],
+      }]
+    };
 
     app.options.postcssOptions = postcssOptions;
+
+    this._super.included.apply(this, arguments);
   },
 };
+
+// Polyfill [Addon._findHost](https://ember-cli.com/api/classes/Addon.html#method__findHost) for older versions of ember-cli
+function findHost(addon) {
+  var current = addon;
+  var app;
+
+  do {
+    app = current.app || app;
+  } while (current.parent.parent && (current = current.parent));
+
+  return app;
+}
