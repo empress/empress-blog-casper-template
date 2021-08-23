1bc0f86391cee58cca62ff9c364d8cb5ae128f61
Chris Manson <chris@manson.ie>
2021-08-23 10:54:54 +0100

    update usage of ember-responsive-image for upgrade
    Co-Authored-By: Chris Manson <mansona@users.noreply.github.com>

--- addon/helpers/responsive-image-url.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function responsiveImageUrl(params/*, hash*/) {
-  return params[0].replace(/^\/images\//, '')
-}
-
-export default helper(responsiveImageUrl);
