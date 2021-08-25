6ee25474807b67d93d481457d7952f7874399e21
Chris Manson <chris@manson.ie>
2021-08-25 10:50:57 +0100

    run lint-to-the-future ignore

--- addon/components/header.js
+++ addon/components/header.js
@@ -1,3 +1,4 @@
+/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier */
 import Component from '@ember/component';
 import layout from '../templates/components/header';
 
--- addon/components/navigation-links.js
+++ addon/components/navigation-links.js
@@ -1,3 +1,4 @@
+/* eslint-disable ember/no-classic-components, ember/no-classic-classes, prettier/prettier */
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import layout from '../templates/components/navigation-links';
