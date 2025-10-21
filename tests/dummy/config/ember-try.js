6e814eba28963b8bb3ac949f3d33a3154bc7d234
Chris Manson <chris@manson.ie>
2025-10-21 14:26:35 +0100

    drop support for ember < 3.28

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -8,32 +8,6 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.0',
-            'ember-data': '~3.20.0',
-            'ember-qunit': '^5.0.0',
-            'ember-cli': '^4.12.0',
-            'ember-resolver': '8.1.0',
-            '@ember/test-helpers': '^2.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.0',
-            'ember-data': '~3.24.0',
-            'ember-qunit': '^5.0.0',
-            'ember-cli': '^4.12.0',
-            'ember-resolver': '8.1.0',
-            '@ember/test-helpers': '^2.4.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
